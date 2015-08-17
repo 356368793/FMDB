ff5b2d19832946e7383566204c61117770c54da8
XiaoC <356368793@qq.com>
2015-08-18 06:57:35 +0800

    storyBoard

---
+++ FMDB/FMDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDB
+//
+//  Created by 肖晨 on 15/8/18.
+//  Copyright (c) 2015年 肖晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FMDB/FMDB/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  FMDB
+//
+//  Created by 肖晨 on 15/8/18.
+//  Copyright (c) 2015年 肖晨. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *ageField;
+- (IBAction)insert;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 打开数据库
+    
+    // 数据库实例对象
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)insert {
+}
+@end
